f48c0c4deb03fe358f4474b9c5dbc602f61a99b3
Edmund Miller <git@edmundmiller.dev>
2024-05-24 12:13:30 -0400

    chore: Fix typo

--- modules/nextflow/src/test/groovy/nextflow/cli/CmdRunTest.groovy
+++ modules/nextflow/src/test/groovy/nextflow/cli/CmdRunTest.groovy
@@ -282,7 +282,7 @@ class CmdRunTest extends Specification {
     }
 
     @Unroll
-    def 'should guss is repo' () {
+    def 'should guess is repo' () {
         expect:
         CmdRun.guessIsRepo(PATH) == EXPECTED
         
