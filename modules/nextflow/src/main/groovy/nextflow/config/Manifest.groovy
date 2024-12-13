70bfe5e145801efa9f0780631061ee9f7dfa408d
Edmund Miller <edmund.miller@seqera.io>
2024-12-13 10:01:41 -0600

    chore: Add logic explanation

--- modules/nextflow/src/main/groovy/nextflow/config/Manifest.groovy
+++ modules/nextflow/src/main/groovy/nextflow/config/Manifest.groovy
@@ -55,6 +55,9 @@ class Manifest {
         target.defaultBranch ?: DEFAULT_BRANCH
     }
 
+    // 1. if defaultRevision is set, use that
+    // 2. if version is set, use that
+    // 3. Fallback to defaultBranch
     String getDefaultRevision() {
         target.defaultRevision ?: getVersion() ?: getDefaultBranch()
     }
